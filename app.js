b3c0773bc58cf476b2ec3652ff1337cfcbceb7b0
Arthurlpgc <arthurlpgcosta@gmail.com>
2016-10-11 16:43:19 -0700

    using ng style

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.controller('MainController',function(){
 		this.area=Carea;
 		document.cookie="area="+Carea+";";
 		this.title=titles[this.area-1];
-		this.scrsize=400;
+		this.scrsize=Math.max(window.innerHeight,document.getElementById('ola').clientHeight);
 	};
 
 	this.isArea=function(Carea){
