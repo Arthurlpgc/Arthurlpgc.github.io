c151a9282638c97716757fdfae730c2c5b4ae1bb
arthur <arthurlpgcosta@gmail.com>
2017-03-28 07:19:47 -0300

    Email

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 var app=angular.module('MainApp',[ ]);
 app.controller('MainController',function(){
-	
+	this.area=2
+	this.changeArea=function(Carea){
+		this.area=Carea;
+	};
+
+	this.isArea=function(Carea){
+		return this.area===Carea;
+	};
 });
 
 app.directive('navbar',function(){
